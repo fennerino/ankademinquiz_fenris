28c072e0837274e243d08266e1275135a1101fc1
Fenris Janghede <fenrisjanghede@outlook.com>
2024-12-03 16:53:19 +0100

    merge-to-vgbranch

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let lockAnswerBtn = document.querySelector("#lockAnswerBtn")
 let proceedBtn = document.getElementById("#proceed-btn")
 
 let ol = document.querySelector("#questionList")
+
 let questionList = document.querySelectorAll("#questionList li")
 
 
