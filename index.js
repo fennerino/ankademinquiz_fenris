8b987448def143ea161a273327447d227715f53f
Fenris Janghede <fenrisjanghede@outlook.com>
2024-12-03 14:46:37 +0100

    sync-pre-PCrestart

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ let lockAnswerBtn = document.querySelector("#lockAnswerBtn")
 let proceedBtn = document.getElementById("#proceed-btn")
 
 let ol = document.querySelector("#questionList")
-let questionList = document.querySelectorAll("li")
+let questionList = document.querySelectorAll("#questionList li")
 
 
 function lockAnswer() {
+  
+}
+
+function submitQuiz() {
   
 }
