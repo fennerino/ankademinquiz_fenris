c8769a83fe38123bbcbf3e61df2018fba8b50206
Fenris Janghede <fenrisjanghede@outlook.com>
2024-12-03 17:35:45 +0100

    pull-from-main

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 
 //let questions = document.querySelectorAll("li"); 
-let questions = document.querySelectorAll("fieldset");
-let lockAnswerBtn = document.querySelector("#lockAnswerBtn")
-let proceedBtn = document.getElementById("#proceed-btn")
+// let questions = document.querySelectorAll("fieldset");
+// let questionList = document.querySelectorAll("#questionList")
 
-let ol = document.querySelector("#questionList")
-let questionList = document.querySelectorAll("#questionList li")
+let questions = document.querySelectorAll("#questionList li");
+
+let lockAnswerBtn = document.querySelectorAll("#lockAnswerBtn").forEach((button, index) => {
+
+});
+
+let proceedBtn = document.querySelector("#proceed-btn");
+
+questions.forEach((question, index) => {
+  console.log(question, index);
+})
 
 
 function lockAnswer() {
