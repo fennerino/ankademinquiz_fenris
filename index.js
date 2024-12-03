fc3b38b84d8a12475d3de2b4980c718998490adc
Fenris Janghede <fenrisjanghede@outlook.com>
2024-12-03 14:07:17 +0100

    more-html+css gen

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//let questions = document.querySelectorAll("li"); 
+let questions = document.querySelectorAll("fieldset");
+let lockAnswerBtn = document.querySelector("#lockAnswerBtn")
+let proceedBtn = document.getElementById("#proceed-btn")
+
+let ol = document.querySelector("#questionList")
+let questionList = document.querySelectorAll("li")
+
+
+function lockAnswer() {
+  
+}
